fcfeae6359b7dbf8116d130d80a2ef5a73905968
BogdanBaesu <35453050+BogdanBaesu@users.noreply.github.com>
2019-04-04 08:46:04 +0300

    Add files via upload

---
+++ baesubogdan_DGDFE.cpp
@@ -0,0 +1,42 @@
+#include <fstream>
+#include <iostream>
+using namespace std;
+
+ifstream fin("input.dat");
+
+int n;
+int G[100][100];
+int S[100];
+int VIZ[100];
+int top;
+int c=0;
+int m=0;
+
+void read_data()
+{
+    fin>>n;
+    for(int i=1; i<=n; i++)
+        for(int j=1; j<=n; j++)
+            fin>>G[i][j];
+}
+
+int DGDFE(int node)
+{
+    cout<<node<<" ";
+    VIZ[node]=1;
+    int adiacent=1;
+    while(adiacent<=n)
+    { if(G[node][adiacent]==1 && VIZ[adiacent]==0)
+    {
+        DGDFE(adiacent);
+    }
+        adiacent++;
+    }
+    return 0;
+}
+
+int main()
+{
+    read_data();
+    DGDFE(1);
+}
